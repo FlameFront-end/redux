7c20d3ba79e630d5f29865f6b7464d9a3989f294
FlameFront-end <5017_30@mail.ru>
2025-06-15 19:10:26 +0300

    react-redux refactor

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,11 @@
-import './styles/App.css'
+import type { FC } from 'react'
 import {
-	type AppState,
-	type CounterId,
 	type DecrementAction,
 	type IncrementAction,
-	store
+	useAppDispatch,
+	useAppSelector
 } from './store.ts'
-import { useEffect, useReducer, useRef } from 'react'
+import './styles/App.css'
 
 function App() {
 	return (
@@ -18,37 +17,27 @@ function App() {
 	)
 }
 
-const selectCounter = (state: AppState, counterId: CounterId) =>
-	state.counters[counterId]
+interface CounterProps {
+	counterId: string
+}
 
-export const Counter = ({ counterId }: { counterId: CounterId }) => {
-	const [, forceUpdate] = useReducer(x => x + 1, 0)
+export const Counter: FC<CounterProps> = ({ counterId }) => {
+	const dispatch = useAppDispatch()
+	const counterState = useAppSelector(state => state.counters[counterId])
 
-	const lastStateRef = useRef<ReturnType<typeof selectCounter> | null>(null)
+	// useSelector вызывается на каждое изменение, исходя из этого,
+	// если мы достанем state, то будет ререндер на каждое изменение, а если мы достанем только то, что нужно (в данный момент только один конкретный counter),
+	// то ререндер будет происходить только после изменение конкретно его
+	// ИТОГ: ДОСТАВАТЬ МАКСИМАЛЬНО МАЛЕНЬКИЙ НУЖНЫЙ КУСОЧЕК STATE и желательно использовать в селекторе сложность О(1), так как очень часто оно перезапускается и не создавать новые ссылки
 
 	console.log(`render ${counterId}`)
-
-	useEffect(() => {
-		return store.subscribe(() => {
-			const currentState = selectCounter(store.getState(), counterId)
-			const lastState = lastStateRef.current
-
-			if (currentState !== lastState) {
-				forceUpdate()
-			}
-
-			lastStateRef.current = currentState
-		})
-	}, [])
-
-	const counterState = selectCounter(store.getState(), counterId)
 
 	return (
 		<div className='card'>
 			<div>counter {counterState?.counter}</div>
 			<button
 				onClick={() =>
-					store.dispatch({
+					dispatch({
 						type: 'increment',
 						payload: {
 							counterId
@@ -56,11 +45,11 @@ export const Counter = ({ counterId }: { counterId: CounterId }) => {
 					} satisfies IncrementAction)
 				}
 			>
-				increment
+				+
 			</button>
 			<button
 				onClick={() =>
-					store.dispatch({
+					dispatch({
 						type: 'decrement',
 						payload: {
 							counterId
@@ -68,7 +57,7 @@ export const Counter = ({ counterId }: { counterId: CounterId }) => {
 					} satisfies DecrementAction)
 				}
 			>
-				decrement
+				-
 			</button>
 		</div>
 	)
--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import './styles/index.css'
+import { Provider } from 'react-redux'
 import App from './App.tsx'
+import { store } from './store.ts'
+import './styles/index.css'
 
-createRoot(document.getElementById('root')!).render(<App />)
+createRoot(document.getElementById('root')!).render(
+	<Provider store={store}>
+		<App />
+	</Provider>
+)
